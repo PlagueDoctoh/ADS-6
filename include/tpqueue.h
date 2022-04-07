fbeedbae16c887b76f30b055eaa4c98d87e44a61
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-04-08 00:06:34 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,25 +1,24 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
-  private:
+ private:
     T arr[100];
-    int first, int last;
-  public:
+    int first, last;
+ public:
     TPQueue() : first(0), last(0) {}
     void push(T x) {
-      if (last - first >= 100) {
-        throw std::string ("Full!");
-      { else {
-          while (( 
-    }
-    T front() {
-        return arr[first % size];
-    }
-    T back() {
-        return arr[(last - 1) % size];
+      if (last - first >= size) {
+        throw std::string("Full!");
+      } else {
+          while (( last >= first) && (arr[(++last) % size).prior <x.prior)) {
+              arr[(++last) % size] = arr[last % size];
+          }
+          arr[(++last) % size] = x;
+      }
     }
     T pop() {
         return arr[(first++) % size];
