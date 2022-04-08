f69e32b3879b38159c63fb3a9bc00c4c439d0895
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-04-08 12:14:53 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -14,7 +14,7 @@ class TPQueue {
       if (last - first >= size) {
         throw std::string("Full!");
       } else {
-          int vrem = --last;
+          int vrem = last--;
           while ((vrem >= first) && (arr[vrem % size].prior < x.prior)) {
               arr[(vrem + 1) % size] = arr[vrem % size];
               --vrem;
